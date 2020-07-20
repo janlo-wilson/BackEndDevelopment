e1054da47bf0d6a58c60e81ee34aff12c32cf3e2
Jan Wilson <janlo.wilson@gmail.com>
2020-07-20 14:42:56 -0700

    OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "282466229633605",
+    clientSecret: "ae164e70346fbd895f86f67e873e30ec",
+  },
 };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const User = require("../models/user");
 const passport = require("passport");
 const authenticate = require("../authenticate");
 const cors = require("./cors");
+const { facebook } = require("../config");
+const { token } = require("morgan");
 
 const router = express.Router();
 
@@ -84,5 +86,22 @@ router.get("/logout", cors.corsWithOptions, (req, res, next) => {
     return next(err);
   }
 });
+
+router.get(
+  "/facebook/token",
+  passport.authenticate("facebook-token"),
+  (req, res) => {
+    if (req.user) {
+      const token = authenticate.getToken({ _id: req.user._id });
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json({
+        success: true,
+        token: token,
+        status: "You are successfully logged in!",
+      });
+    }
+  }
+);
 
 module.exports = router;
