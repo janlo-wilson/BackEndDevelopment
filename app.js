d49a01a1715c96560e400f2b51815e4a5e1a1cc0
Jan Wilson <janlo.wilson@gmail.com>
2020-07-25 14:29:47 -0700

    Workshop 4

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const capsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 const uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favoriteRouter");
 
 const mongoose = require("mongoose");
 
@@ -63,6 +64,7 @@ app.use("/campsites", capsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
 app.use("/imageUpload", uploadRouter);
+app.use("/favorites", favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
